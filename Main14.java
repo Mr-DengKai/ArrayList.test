f46de495068897f6ce7adf2b1455c7ff23d556bc
邓凯 <964809903@qq.com>
2016-06-16 17:31:15 +0800

    Create Main14.java

---
+++ Main14.java
@@ -0,0 +1,20 @@
+package com.orilore.dk.test1;
+import java.util.*;
+
+import java.util.Iterator;
+
+
+public class Main14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+HashSet<String> list =new HashSet<String>();
+list.add("hello");
+list.add("java");
+list.add("world");
+for(String str:list){
+	
+System.out.println(str);
+	}
+	}
+}
