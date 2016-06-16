e651c28a04665a31d910aa48c80c30a563006e62
Mr-DengKai <964809903@qq.com>
2016-06-16 17:39:47 +0800

    add Main.java

---
+++ Main18.java
@@ -0,0 +1,14 @@
+package com.orilore.dk.test1;
+import java.util.*;
+public class Main18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+TreeSet<Integer> set = new TreeSet<Integer>();
+set.add(10);
+set.add(9);
+set.add(8);
+System.out.println(set);  //--------Ë³ĞòÊä³ö
+	}
+
+}
