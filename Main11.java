b798eb84d65103197741d54ca7dcd8973435e75a
邓凯 <964809903@qq.com>
2016-06-16 10:07:41 +0800

    Main11.java
    add Main.java

---
+++ Main11.java
@@ -0,0 +1,18 @@
+package com.orilore.dk.test1;
+import java.util.*;
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+LinkedList list = new LinkedList();
+LinkedList list2 = new LinkedList();
+LinkedList list3 = new LinkedList();
+
+list.add(1);
+list.add(4);
+list2.add(2);
+list.addLast(2);
+System.out.println(list);
+	}
+
+}
