939cf61abf7057cae86706a1e993e7e3157947c3
Mr-DengKai <964809903@qq.com>
2016-06-16 17:22:17 +0800

    add Main.java

---
+++ Main13.java
@@ -0,0 +1,14 @@
+package com.orilore.dk.test1;
+import java.util.*;
+public class Main13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+HashSet list = new HashSet();
+list.add(10);
+list.add(11);
+list.add(9);
+System.out.println(list);
+	}
+
+}
